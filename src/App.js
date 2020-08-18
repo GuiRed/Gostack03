d3bb8166cee126e6073b99bcf72106f5b75dbd45
Guilherme Locks <mr.glocks@gmail.com>
2020-08-18 20:46:36 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  const getRepositories = () => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  };
+
+  useEffect(() => {
+    getRepositories();
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Teste título - ${Date.now()}`,
+      owner: 'Guilherme Locks'
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`)
+      .then(() => {
+        getRepositories();
+      })
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
         <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
+          <ul>
+            {repositories.map(repository => 
+              <li key={repository.id}>
+                {repository.title}
+                <button onClick={() => handleRemoveRepository(repository.id)}>
+                  Remover
+                </button>
+              </li>)}
+          </ul>
         </li>
       </ul>
 
